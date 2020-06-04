aa24bd781dbcb2a43e46392884465128865a4858
Cian O'Gralaigh <ihatecian@gmail.com>
2020-06-04 17:37:21 +0100

    Add an operator expression

--- src/Hello.java
+++ src/Hello.java
@@ -1,7 +1,7 @@
 public class Hello {
 
     public static void main(String[] args) {
-        int myFirstNumber = 5;
+        int myFirstNumber = (5 * 5) + 5;
 
         System.out.println(myFirstNumber);
     }
