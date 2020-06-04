07354278891a17f4265f683e318c3f881815b6af
Cian O'Gralaigh <ihatecian@gmail.com>
2020-06-04 17:40:02 +0100

    Add more variables to the Hello class

--- src/Hello.java
+++ src/Hello.java
@@ -2,8 +2,10 @@ public class Hello {
 
     public static void main(String[] args) {
         int myFirstNumber = (5 * 5) + 5;
+        int mySecondNumber = 12;
+        int myThirdNumber = 6;
 
-        System.out.println(myFirstNumber);
+        System.out.println(myFirstNumber + mySecondNumber + myThirdNumber);
     }
 
 
