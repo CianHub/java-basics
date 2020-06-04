655615acb82f2d176aaf01105cbef2db04cc72b9
Cian O'Gralaigh <ihatecian@gmail.com>
2020-06-04 17:33:43 +0100

    Add a variable to the Hello class

--- src/Hello.java
+++ src/Hello.java
@@ -1,5 +1,10 @@
 public class Hello {
-    public  static void main(String[] args) {
-        System.out.println(("Hello World"));
+
+    public static void main(String[] args) {
+        int myFirstNumber = 5;
+
+        System.out.println(myFirstNumber);
     }
+
+
 }
